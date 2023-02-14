a946c74f17788bfba529a6664cf6739126a21950
Raisa Myronova <raisam@rubitax.com>
2023-02-14 12:37:15 +0200

    add stylelint, i18n

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,46 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "amd": true,
-        "node": true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:i18next/recommended',
     ],
-    "overrides": [
+    overrides: [
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'i18next'
     ],
-    "rules": {
+    rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension':[2, { extensions:['.js', '.jsx', '.tsx']}],
-        'import/no-unresolved':'off',
-        'import/prefer-default-export':'off',
-        'no-unused-vars':'warn',
-        'react/require-default-props':'off',
-        'react/jsx-props-no-spreading':'warn',
-        'react/function-component-definition':'off',
-        'no-shadow':'off',
-        'import/extensions':'off',
-        'import/no-extraneous-dependencies':'off',
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        'react/require-default-props': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'react/function-component-definition': 'off',
+        'no-shadow': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        "react/prop-types": "off",
-        "react/react-in-jsx-scope": "off",
-        'no-useless-escape':'off'
-    },
-    globals: {
-        __IS_DEV__:true,
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'no-useless-escape': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        
     }
 };
