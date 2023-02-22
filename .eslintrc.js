82a9e0beed6561e5d8f4907990db5ed715e0e114
Raisa Myronova <raisam@rubitax.com>
2023-02-22 11:00:00 +0200

    fix err eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,64 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
-  overrides: [],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['react', '@typescript-eslint', 'i18next'],
-  rules: {
-    'react/jsx-indent': [2, 4],
-    'react/jsx-indent-props': [2, 4],
-    indent: [2, 4],
-    'react/jsx-filename-extension': [2, {
-      extensions: ['.js', '.jsx', '.tsx']
-    }],
-    'import/no-unresolved': 'off',
-    'import/prefer-default-export': 'off',
-    'no-unused-vars': 'warn',
-    'react/require-default-props': 'off',
-    'react/jsx-props-no-spreading': 'warn',
-    'react/function-component-definition': 'off',
-    'no-shadow': 'off',
-    'import/extensions': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'no-underscore-dangle': 'off',
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'no-useless-escape': 'off',
-    'i18next/no-literal-string': ['error', {
-      markupOnly: true,
-      ignoreAttribute: ['data-testid', 'to']
-    }],
-    "no-empty-function": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    globals: {
-      __IS_DEV__: true
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
     },
-    overrides: [{
-      files: ['**/src/**/*.test.{ts,tsx}'],
-      rules: {
-        'i18next/no-literal-string': 'off'
-      }
-    }]
-  }
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'i18next',
+    ],
+    rules: {
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        indent: [2, 4],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        'react/require-default-props': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'react/function-component-definition': 'off',
+        'no-shadow': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': [
+            'error',
+            {
+                markupOnly: true,
+                ignoreAttribute: ['data-testid', 'to'],
+            },
+        ],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
+    },
+    globals: {
+        __IS_DEV__: true,
+    },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
