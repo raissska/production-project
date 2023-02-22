4715ec974441ce63ed6629ec6d09fd214816d244
Raisa Myronova <raisam@rubitax.com>
2023-02-22 10:42:34 +0200

    add declared module for svg ts, checked buds AboutPage.stories and MainPage.stories

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,12 @@
 import webpack from 'webpack';
-import { BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
-
+import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-      };
+    };
 
     const babelLoader = {
         test: /\.(js|jsx|tsx)$/,
@@ -29,9 +28,9 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         },
     };
 
-    const cssLoader = buildCssLoader(isDev)
+    const cssLoader = buildCssLoader(isDev);
 
-    // Если не используем тайпскрипт - нужен babel-loader
+   
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,8 +1,8 @@
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './ThemeSwitcher.module.scss';
-import LightIcon from 'shared/assets/icons/theme-light.svg'
-import DarktIcon from 'shared/assets/icons/theme-dark.svg'
+import LightIcon from '../../../assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 interface ThemeSwitcherProps {
@@ -20,7 +20,7 @@ export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = (props) => {
             className={classNames(cls.themeSwitcher, {}, [className])}
             onClick={toggleTheme}
         >
-            {theme === Theme.DARK ? <DarktIcon/> : <LightIcon/>}
+            {theme === Theme.DARK ? <DarkIcon/> : <LightIcon/>}
         </Button>
     );
 }
