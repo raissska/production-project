505417f8cb11835f717879bd36da3d8e31afd3fc
Raisa Myronova <raisam@rubitax.com>
2023-03-01 22:05:44 +0200

    fix screenshots

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,19 +1,21 @@
-import { addDecorator } from "@storybook/react"
-import {StoryDecorator} from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator'
-import {ThemeDecorator} from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator'
-import {RouterDecorator} from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator'
-import {Theme} from '../../src/app/providers/ThemeProvider'
+import { addDecorator } from '@storybook/react';
+import { StoryDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { StoreDecorator } from '../../src/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+};
 
-addDecorator(StoryDecorator)
-addDecorator(ThemeDecorator(Theme.LIGHT))
-addDecorator(RouterDecorator)
+addDecorator(StoreDecorator({ value: 0 }));
+addDecorator(StoryDecorator);
+addDecorator(ThemeDecorator(Theme.LIGHT));
+addDecorator(RouterDecorator);
