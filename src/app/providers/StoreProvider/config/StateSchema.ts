992e8e427a8a69698d375463c4eef405a04db568
Raisa Myronova <raisam@rubitax.com>
2023-03-12 18:51:27 +0200

    add lazy loading, add reducer manager, add dynamic module loader component

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,3 +1,7 @@
+import {
+    EnhancedStore,
+    AnyAction, Reducer, ReducersMapObject, CombinedState,
+} from '@reduxjs/toolkit';
 import { CounterSchema } from 'entities/Counter/model/types/counterSchema';
 import { UserScheme } from 'entities/User';
 import { LoginSchema } from 'features/AuthByUsername';
@@ -5,5 +9,20 @@ import { LoginSchema } from 'features/AuthByUsername';
 export interface StateSchema {
    counter: CounterSchema;
    user: UserScheme;
-   login: LoginSchema;
+   login?: LoginSchema;
+}
+
+export type StateSchemaKey = keyof StateSchema;
+
+export interface ReducerManager {
+   getReducerMap: () => ReducersMapObject<StateSchema>;
+   reduce: (state: StateSchema, action: AnyAction) => CombinedState<StateSchema>;
+   add: (key: StateSchemaKey, reducer: Reducer) => void;
+   remove: (key: StateSchemaKey) => void;
+
+ }
+
+export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
+
+   reducerManager: ReducerManager;
 }
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,28 +1,36 @@
-import { getLoginState } from 'features/AuthByUsername/model/selectors/getLoginState/getLoginState';
+import { getLoginError } from 'features/AuthByUsername/model/selectors/getLoginError/getLoginError';
+import { getLoginIsLoading } from 'features/AuthByUsername/model/selectors/getLoginIsLoading/getLoginIsLoading';
+import { getLoginPassword } from 'features/AuthByUsername/model/selectors/getLoginPassword/getLoginPassword';
+import { getLoginUsername } from 'features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername';
 import { loginByUsername } from 'features/AuthByUsername/model/services/loginByUsername/loginByUsername';
-import { loginActions } from 'features/AuthByUsername/model/slice/loginSlice';
+import { loginActions, loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 import React, { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
 import { Text, TextTheme } from 'shared/ui/Text/Text';
 import cls from './LoginForm.module.scss';
 
-interface LoginFormProps {
+export interface LoginFormProps {
   className?: string;
 
 }
 
-export const LoginForm = memo((props: LoginFormProps) => {
+const initialReducers: ReducersList = {
+    login: loginReducer,
+};
+
+const LoginForm = memo((props: LoginFormProps) => {
     const { className } = props;
     const { t } = useTranslation();
     const dispatch = useDispatch();
-    const loginForm = useSelector(getLoginState);
-    const {
-        username, password, error, isLoading,
-    } = loginForm;
+    const username = useSelector(getLoginUsername);
+    const password = useSelector(getLoginPassword);
+    const error = useSelector(getLoginError);
+    const isLoading = useSelector(getLoginIsLoading);
 
     const onChangeUsername = useCallback((value: string) => {
         dispatch(loginActions.setUsername(value));
@@ -37,33 +45,37 @@ export const LoginForm = memo((props: LoginFormProps) => {
     }, [dispatch, username, password]);
 
     return (
-        <div className={classNames(cls.loginForm, {}, [className])}>
-            <h2>{t('login')}</h2>
-            {/* <Text theme={TextTheme.ERROR} title={t('login')} /> */}
-            {error && <Text theme={TextTheme.ERROR} text={t('You entered an incorrect username or password')} />}
-            <Input
-                placeholder={t('userName')}
-                autofocus
-                type="text"
-                className={cls.input}
-                onChange={onChangeUsername}
-                value={username}
-            />
-            <Input
-                placeholder={t('password')}
-                type="text"
-                className={cls.input}
-                onChange={onChangePassword}
-                value={password}
-            />
-            <Button
-                theme={ButtonTheme.BACKROUND_INVERTED}
-                className={cls.loginBtn}
-                onClick={onLoginClick}
-                disabled={isLoading}
-            >
-                {t('login')}
-            </Button>
-        </div>
+        <DynamicModuleLoader removeAfterUnmount reducers={initialReducers}>
+            <div className={classNames(cls.loginForm, {}, [className])}>
+                <h2>{t('login')}</h2>
+                {/* <Text theme={TextTheme.ERROR} title={t('login')} /> */}
+                {error && <Text theme={TextTheme.ERROR} text={t('You entered an incorrect username or password')} />}
+                <Input
+                    placeholder={t('userName')}
+                    autofocus
+                    type="text"
+                    className={cls.input}
+                    onChange={onChangeUsername}
+                    value={username}
+                />
+                <Input
+                    placeholder={t('password')}
+                    type="text"
+                    className={cls.input}
+                    onChange={onChangePassword}
+                    value={password}
+                />
+                <Button
+                    theme={ButtonTheme.BACKROUND_INVERTED}
+                    className={cls.loginBtn}
+                    onClick={onLoginClick}
+                    disabled={isLoading}
+                >
+                    {t('login')}
+                </Button>
+            </div>
+        </DynamicModuleLoader>
     );
 });
+
+export default LoginForm;
