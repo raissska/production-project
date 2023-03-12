852900b7a9b629f3d7b1b82d0506e0f4a683fee6
Raisa Myronova <raisam@rubitax.com>
2023-03-12 19:11:20 +0200

    add lazy loading, add reducer manager, add dynamic module loader component

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,9 +1,17 @@
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
-import { DeepPartial } from '@reduxjs/toolkit';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 
-export const StoreDecorator = (state: DeepPartial<StateSchema>) => (StoryComponent: Story) => (
-    <StoreProvider initialState={state}>
+const defaultAsyncReducer: DeepPartial<ReducersMapObject<StateSchema>> = {
+    login: loginReducer,
+};
+
+export const StoreDecorator = (
+    state: DeepPartial<StateSchema>,
+    asyncReducer?: DeepPartial<ReducersMapObject<StateSchema>>,
+) => (StoryComponent: Story) => (
+    <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducer, ...asyncReducer }}>
         <StoryComponent />
     </StoreProvider>
 );
