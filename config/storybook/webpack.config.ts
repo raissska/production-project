7a33772a902d0d7241d9564f4cda9929641410e2
Raisa Myronova <raisam@rubitax.com>
2023-03-24 14:23:20 +0300

    fix bugs with loki

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -13,6 +13,13 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config!.resolve!.modules!.push(paths.src);
     config!.resolve!.extensions!.push('.ts', '.tsx');
 
+    if (config!.resolve!.modules) {
+        config!.resolve!.modules = [
+            path.resolve(__dirname, '../../src'),
+            'node_modules',
+        ];
+    }
+
     // eslint-disable-next-line no-param-reassign
     // @ts-ignore
     config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
