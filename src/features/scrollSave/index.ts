73c069b9c8414bdcdcdb3789ef5c379d41f2ad90
Raisa Myronova <raisam@rubitax.com>
2023-04-04 11:00:58 +0300

    fix unit testing

--- src/features/scrollSave/index.ts
+++ src/features/scrollSave/index.ts
@@ -1,3 +1,3 @@
 export { ScrollSaveSchema } from './model/types/ScrollSaveSchema';
 export { getScrollSaveByPath } from './model/selectors/getScrollSave';
-export { scrollSaveReducer, scrollSaveActions } from './model/slices/scrollsaveSlice';
+export { scrollSaveReducer, scrollSaveActions } from './model/slices/scrollSaveSlice';
