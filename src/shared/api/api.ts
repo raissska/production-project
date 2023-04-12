b44ab6a45fe8fd3d5581f7e1402d4df9468b394b
Raisa Myronova <raisam@rubitax.com>
2023-04-12 13:12:54 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,10 +1,13 @@
-// import { __API__ } from 'app/types/global';
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
